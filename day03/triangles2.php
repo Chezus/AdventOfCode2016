8c161543fa6da0b7be2b7bea39adfa7de5ad3eec
Kevin <kevin@yaaaaaay.me>
2018-01-03 22:38:39 +0100

    Forgot some debugging code

--- day03/triangles2.php
+++ day03/triangles2.php
@@ -8,8 +8,7 @@ $triangles = array_map(function ($n) {
 }, $input);
 
 $counter = 0;
-$i = 0;
-for (; $i+3 <= count($triangles); $i+=3) {
+for ($i = 0; $i+3 <= count($triangles); $i+=3) {
     for ($j = 0; $j < 3; $j++) {
         $side1 = $triangles[$i][0][$j];
         $side2 = $triangles[$i + 1][0][$j];
